e4c4b2eb062f85c84e9537d146fd0730e623e609
Josh Smith <smithjosh024@gmail.com>
2019-03-07 21:12:42 -0700

    added class methods

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-class {
+const SHA256 = require('crypto-js/sha256')
+
+class Block{
     constructor(index, timestamp, data, previousHash = ''){
-        
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+}
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0,"01/01/2019", "Genesis Block", "0")
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
     }
 }
+
+let Tabithereum = new BlockChain();
+Tabithereum.addBlock(new Block(1,"01/02/2019", {amount: 4}));
+Tabithereum.addBlock(new Block(2,"01/03/2019", {amount: 10}));
+
+console.log(JSON.stringify(Tabithereum, null, 4));
