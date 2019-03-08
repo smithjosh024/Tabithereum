06d0205737a2a0a619856a2e51999954f7993247
Josh Smith <smithjosh024@gmail.com>
2019-03-07 21:30:40 -0700

    added mining

--- main.js
+++ main.js
@@ -7,17 +7,27 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")) {
+            this.hash = this.calculateHash();   
+            this.nonce++;
+        }
+
+        console.log("Block Mined: " + this.hash);
+    }
 }
 
 class BlockChain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
     }
 
     createGenesisBlock() {
@@ -25,18 +35,43 @@ class BlockChain {
     }
 
     getLatestBlock() {
-        return this.chain[this.chain.length - 1]
+        return this.chain[this.chain.length - 1];
     }
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            } 
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
 let Tabithereum = new BlockChain();
+
+console.log('Mining Black 1...');
 Tabithereum.addBlock(new Block(1,"01/02/2019", {amount: 4}));
+
+console.log('Mining Black 2...');
 Tabithereum.addBlock(new Block(2,"01/03/2019", {amount: 10}));
 
-console.log(JSON.stringify(Tabithereum, null, 4));
+
+
+// Tabithereum.chain[1].data = {amount: 100};
+// Tabithereum.chain[1].hash = Tabithereum.chain[1].calculateHash();
+
+// console.log('Is blockchain valid? ' + Tabithereum.isChainValid());
+// console.log(JSON.stringify(Tabithereum, null, 4));
